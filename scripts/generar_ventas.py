6749402d591bddd83c8faf36ec014801cfba36b7
Vinbu <vincent.14.damian@gmail.com>
2025-05-20 13:13:56 -0400

    Valores null actualizados en entradas_zoo.csv

--- scripts/generar_ventas.py
+++ scripts/generar_ventas.py
@@ -33,22 +33,35 @@ def dia_semana(fecha):
 diccionario_ventas = {"VIP": [], "rango_etario": [], "fecha": [], "día_semana": [], "hora_ingreso": []}
 
 for _ in range(200):
-    definir_nulo = random.randint(1, 20)
-    if definir_nulo in range(1,3):
+    definir_nulo = random.randint(1, 400)
+    fecha = ""
+    if definir_nulo in range(1, 41):
         diccionario_ventas["VIP"].append(None)
     else:
         diccionario_ventas["VIP"].append(random.choice(["Si", "No"]))
-    diccionario_ventas["rango_etario"].append(random.choice(["Niño", "Adulto", "Adulto Mayor"]))
-    fecha = fecha_random("01/01/2025", "20/05/2025", "%d/%m/%Y", random.random())
-    diccionario_ventas["fecha"].append(fecha)
-    diccionario_ventas["día_semana"].append(dia_semana(fecha))
-    diccionario_ventas["hora_ingreso"].append(hora_random().strftime("%H:%M"))
+    if definir_nulo in range(41, 81):
+        diccionario_ventas["rango_etario"].append(None)
+    else:
+        diccionario_ventas["rango_etario"].append(random.choice(["Niño", "Adulto", "Adulto Mayor"]))
+    if definir_nulo in range(81, 121):
+        diccionario_ventas["fecha"].append(None)
+    else:
+        fecha = fecha_random("01/01/2025", "20/05/2025", "%d/%m/%Y", random.random())
+        diccionario_ventas["fecha"].append(fecha)
+    if definir_nulo in range(121, 161) or fecha == "":
+        diccionario_ventas["día_semana"].append(None)
+    else:
+        diccionario_ventas["día_semana"].append(dia_semana(fecha))
+    if definir_nulo in range(161, 201):
+        diccionario_ventas["hora_ingreso"].append(None)
+    else:
+        diccionario_ventas["hora_ingreso"].append(hora_random().strftime("%H:%M"))
     
 frame = pd.DataFrame(diccionario_ventas, columns=diccionario_ventas.keys())
-
+frame.index.name = "ID_entrada"
 if not os.path.exists(carpeta_destino):
     os.makedirs(carpeta_destino)
     
 nombre_archivo_csv = os.path.join(carpeta_destino, nombre_archivo_csv)
 
-frame.to_csv(nombre_archivo_csv, index=False, na_rep='NULL')
+frame.to_csv(nombre_archivo_csv, na_rep='NULL')
